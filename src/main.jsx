f98b61cde851ad31c9e781eb2e16a7a6d97c6b95
youness <younesserbai@gmail.com>
2024-10-06 16:46:19 +0200

    fix somme somme bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import Register from './pages/Auth/page.jsx'; // Your Register page
+import Register from './pages/Auth/Register.jsx'; // Your Register page
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
