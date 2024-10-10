e6dc5eca4f44e6da7f8a3dfc85c32c00e298940a
youness <younesserbai@gmail.com>
2024-10-10 16:41:11 +0200

    the the  bug of refresh token

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_HOST,
+    baseURL: import.meta.env.VITE_API_HOST, // Assurez-vous que ceci est bien défini, par exemple 'http://localhost:3500'
 })
 
-// Add a request interceptor to add the token to the headers
+// Ajout d'un interceptor pour ajouter le token aux headers
 axiosInstance.interceptors.request.use(
     (config) => {
-        // config.withCredentials = true
+        config.withCredentials = true // Activation de l'envoi des credentials
         const token = localStorage.getItem('token')
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
@@ -19,18 +19,18 @@ axiosInstance.interceptors.request.use(
     }
 )
 
-// Add a response interceptor to handle the token expiration
+// Ajout d'un interceptor pour gérer l'expiration du token
 axiosInstance.interceptors.response.use(
     (response) => {
         return response
     },
     async (error) => {
         const originalRequest = error.config
-        if (error.response.status === 403 && !originalRequest._retry) {
+        if (error.response && error.response.status === 403 && !originalRequest._retry) {
             originalRequest._retry = true
             try {
                 const response = await axiosInstance.get('/refresh')
-                const data = await response.data
+                const data = response.data
                 localStorage.setItem('token', data.accessToken)
                 return axiosInstance(originalRequest)
             } catch (error) {
@@ -42,3 +42,71 @@ axiosInstance.interceptors.response.use(
 )
 
 export default axiosInstance
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from 'axios'
+
+// const axiosInstance = axios.create({
+//     baseURL: import.meta.env.VITE_API_HOST,
+// })
+
+// // Add a request interceptor to add the token to the headers
+// axiosInstance.interceptors.request.use(
+//     (config) => {
+//         // config.withCredentials = true
+//         const token = localStorage.getItem('token')
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`
+//         }
+//         return config
+//     },
+//     (error) => {
+//         return Promise.reject(error)
+//     }
+// )
+
+// // Add a response interceptor to handle the token expiration
+// axiosInstance.interceptors.response.use(
+//     (response) => {
+//         return response
+//     },
+//     async (error) => {
+
+//         const originalRequest = error.config
+//         if (error.status === 403 && !originalRequest._retry) {
+//             originalRequest._retry = true
+//             try {
+//                 const response = await axiosInstance.get('/refresh')
+//                 const data = await response.data
+//                 localStorage.setItem('token', data.accessToken)
+//                 return axiosInstance(originalRequest)
+//             } catch (error) {
+//                 return Promise.reject(error)
+//             }
+//         }
+//         return Promise.reject(error)
+//     }
+// )
+
+// export default axiosInstance
+
+
+
+
+
+
+
+
--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -1,31 +1,71 @@
-
 import { useEffect } from "react";
 import axiosInstance from "../../config/axios";
 import Logout from "../Auth/Logout";
-function Home() {
-console.log("hello")
-useEffect(() => {
 
-    const fetchData = async () => {
-        try {
-            const response = await axiosInstance.get('/home')
-            console.log('Data sent:', response);
-        } catch (error) {
-            console.error('Error sending data:', error);
-        }
-    }
-    fetchData()
-    
-}
-, [])
+function Home() {
+    console.log("Home component mounted");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosInstance.get('/home');
+                console.log('Data received:', response.data);
+            } catch (error) {
+                if (error.response) {
+                    // Le serveur a répondu avec un statut autre que 2xx
+                    console.error('Error Response:', error.response.data);
+                } else if (error.request) {
+                    // La requête a été faite mais aucune réponse n'a été reçue
+                    console.error('No Response:', error.request);
+                } else {
+                    // Une autre erreur est survenue lors de la configuration de la requête
+                    console.error('Error:', error.message);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
-            hello in page dashboard
-            
-
-            <Logout/>
+            <h1>Hello in Page Dashboard</h1>
+            <Logout />
         </div>
     );
 }
 
 export default Home;
+
+
+
+// import { useEffect } from "react";
+// import axiosInstance from "../../config/axios";
+// import Logout from "../Auth/Logout";
+// function Home() {
+// console.log("hello")
+// useEffect(() => {
+
+//     const fetchData = async () => {
+//         try {
+//             const response = await axiosInstance.get('/home')
+//             console.log('Data sent:', response);
+//         } catch (error) {
+//             console.error('Error sending data:', error);
+//         }
+//     }
+//     fetchData()
+    
+// }
+// , [])
+//     return (
+//         <div>
+//             hello in page dashboard
+            
+
+//             <Logout/>
+//         </div>
+//     );
+// }
+
+// export default Home;
